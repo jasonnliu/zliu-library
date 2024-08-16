38835501f9b58ebb1ac0a686b7ab89d6953f7be9
ZhanxuLiu <zliu0257@student.monash.edu>
2024-08-16 15:24:02 +1000

    Finish the content of week3 and week4.

--- zliu-library/src/components/Form.vue
+++ zliu-library/src/components/Form.vue
@@ -7,18 +7,18 @@
             <div class="row mb-3">
               <div class="col-sm-6">
                 <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username"
-                  @blur="() => validateName(true)"
-                  @input="() => validateName(false)"
-                  v-model="formData.username">
-                  <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
+                <input type="text" class="form-control" id="username" 
+                @blur="() => validateName(true)"
+                @input="() => validateName(false)"
+                v-model="formData.username">
+                <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
               </div>
               <div class="col-sm-6">
                 <label for="password" class="form-label">Password</label>
                 <input type="password" class="form-control" id="password"
-                  @blur="() => validatePassword(true)"
-                  @input="() => validatePassword(false)"
-                  v-model="formData.password">
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
+                v-model="formData.password">
                 <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
               </div>
             </div>
@@ -31,16 +31,24 @@
               </div>
               <div class="col-sm-6">
                 <label for="gender" class="form-label">Gender</label>
-                <select class="form-select" id="gender" v-model="formData.gender">
+                <select class="form-select" id="gender" 
+                @blur="() => validateGender(true)" 
+                @input="() => validateGender(false)" 
+                v-model="formData.gender">
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                   <option value="other">Other</option>
                 </select>
+                <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
               </div>
             </div>
             <div class="mb-3">
               <label for="reason" class="form-label">Reason for joining</label>
-              <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
+              <textarea class="form-control" id="reason" rows="3" 
+              @blur="() => validateReason(true)" 
+              @input="() => validateReason(false)" 
+              v-model="formData.reason"></textarea>
+              <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
             </div>
             <div class="text-center">
               <button type="submit" class="btn btn-primary me-2">Submit</button>
@@ -51,6 +59,15 @@
       </div>
     </div>
     <div class="row mt-5" v-if="submittedCards.length">
+        <div class="card">
+            <DataTable :value="submittedCards" tableStyle="min-width: 50rem">
+                <Column field="username" header="Username"></Column>
+                <Column field="password" header="Password"></Column>
+                <Column field="isAustralian" header="isAustralian"></Column>
+                <Column field="gender" header="Gender"></Column>
+                <Column field="reason" header="Reason"></Column>
+            </DataTable>
+        </div>
         <div class="d-flex flex-wrap justify-content-start">
             <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
                 <div class="card-header">
@@ -72,6 +89,8 @@
 
 <script setup>
 
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 import { ref } from 'vue';
   
   const formData = ref({
@@ -87,9 +106,11 @@ import { ref } from 'vue';
   
   
   const submitForm = () => {
-    validateName(true);
-    validatePassword(true);
-    if (!errors.value.username && !errors.value.password) {
+    validateName(true)
+    validatePassword(true)
+    validateGender(true)
+    validateReason(true)
+    if (!errors.value.username && !errors.value.password && !errors.value.gender && !errors.value.reason) {
       submittedCards.value.push({ ...formData.value });
       clearForm();
     }
@@ -134,6 +155,22 @@ import { ref } from 'vue';
     errors.value.password = null;
   }
 };
+
+  const validateGender = (blur) => {
+    if (!formData.value.gender) {
+      if (blur) errors.value.gender = "Gender should be selected.";
+    } else {
+      errors.value.gender = null;
+    }
+  };
+
+  const validateReason = (blur) => {
+    if (formData.value.reason.length < 5) {
+      if (blur) errors.value.reason = "Reason should be at least 5 characters.";
+    } else {
+      errors.value.reason = null;
+    }
+  };
 
 
 
